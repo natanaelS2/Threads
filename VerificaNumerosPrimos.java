00effc034511dfd910b0d3d6f3f86955ae042bd7
natanael santos <https://github.com/natanaelS2/Threads.git>
2023-08-13 00:22:23 -0300

    threadsNumPrimos

---
+++ VerificaNumerosPrimos.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+public class VerificaNumerosPrimos {
+    public static void main(String[] args) {
+        List<Thread> threads = new ArrayList<>();
+
+        for (String arg : args) {
+            int numero = Integer.parseInt(arg);
+            Thread n = new Thread(() -> {
+                if (isNumeroPrime(numero)) {
+                    System.out.println("O número " + numero + " é primo.");
+                } else {
+                    System.out.println("O número " + numero + " não é primo.");
+                }
+            });
+            threads.add(n);
+            n.start();
+        }
+        for (Thread n : threads) {
+            try {
+                n.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static boolean isNumeroPrime(int numero) {
+        if (numero <= 1) return false;
+        int i;
+        for ( i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) return false;
+        }
+        return true;
+    }
+}
